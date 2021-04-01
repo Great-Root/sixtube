0e2c502f53c63db612692468df65dc57883f9311
Great-Root <icujoa@gmail.com>
2021-04-01 20:48:05 +0900

    비디오 플레이어 재생바 구현

--- src/video/VideoController.java
+++ src/video/VideoController.java
@@ -23,52 +23,40 @@ public class VideoController implements Initializable {
 	}
 	
 	public void playProc() {
-		System.out.println("플레이");
 		service.playProc();
 	}
 	public void pauseProc() {
-		System.out.println("일시정지");
 		service.pauseProc();
 	}
 	public void stopProc() {
-		System.out.println("정지");
 		service.stopProc();
 	}
 	public void volumnDragProc() {
-		System.out.println("볼륨 드래그");
 		service.volumnDragProc();
 	}
 	public void volumnClickProc() {
-		System.out.println("볼륨 클릭");
 		service.volumnClickProc();
 	}
 	public void timeDragProc() {
-		System.out.println("영상시간 드래그");
 		service.timeDragProc();
 	}
 	public void timeClickProc() {
-		System.out.println("영상시간 클릭");
 		service.timeClickProc();
 	}
 	public void plusProc() {
-		System.out.println("10초 후");
 		service.plusProc();
 	}
 	public void minusProc () {
-		System.out.println("10초 전");
 		service.minusProc();
 	}
 	public void slowProc() {
-		System.out.println("0.5배속");
 		service.slowProc();
 	}
 	public void fastProc() {
-		System.out.println("2배속");
 		service.fastProc();
 	}
 	public void setVideo(Parent root, String vpath) {
 		service.setRoot(root);
-		System.out.println(vpath);
 		service.setVideo(vpath);
 	}
 	
